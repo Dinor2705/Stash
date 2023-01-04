bd0259037d3bb500e6548cffb04f36bd01762087
DinorCyrus <112527752+Dinor2705@users.noreply.github.com>
2023-01-04 21:16:15 +0700

    Update IP-Check.js
    Signed-off-by: DinorCyrus <112527752+Dinor2705@users.noreply.github.com>

--- IP-Check.js
+++ IP-Check.js
@@ -1,36 +1,37 @@
 /*
- * 由@congcong0806编写
- * 原脚本地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
- * 由@Rabbit-Spec修改
- * 更新日期：2022.08.14
- * 版本：1.5
- */
+https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/ipcheck.js
 
-let url = "http://ip-api.com/json"
+* [Panel]
+* 网络信息 = script-name=网络信息, title="网络信息", content="请刷新", style=info, update-interval=60
+* ...
+* [Script]
+* 网络信息 = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/ipcheck.js
+*/
+
+let url = "http://ip-api.com/json/?lang=zh-VN"
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
+    let ip = jsonData.query
     let country = jsonData.country
     let emoji = getFlagEmoji(jsonData.countryCode)
     let city = jsonData.city
     let isp = jsonData.isp
-    let ip = jsonData.query
+    
   body = {
-    title: "Node information",
-    content: `IP：${ip}\nOperator：${isp}\nSite：${emoji}${country} - ${city}`,
-    icon: "globe.asia.australia.fill"
+    title: "INFO",
+    content: `IP: ${ip}\n运营商: ${isp}\nIP位置: ${emoji}${country} - ${city}`,
+    icon: "link.icloud",
+    'icon-color': "#5AC8FA"
   }
   $done(body);
 });
 
-function getFlagEmoji(countryCode) {
-      if (countryCode.toUpperCase() == 'VN') {
-    countryCode = 'VN'
-  }
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt())
-  return String.fromCodePoint(...codePoints)
 
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
 }
